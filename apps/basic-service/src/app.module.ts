b8f802b987ff609684357583b272c4a11cb5a992
luo <2904133050@qq.com>
2023-03-10 13:41:02 +0800

    fix: fix project error

--- apps/basic-service/src/app.module.ts
+++ apps/basic-service/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/user.entity';
 
 @Module({
   imports: [
